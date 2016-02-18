df93ae0427f047b81ffda659646f15556828d77d
bhanu <bhanu@impingeonline.com>
2016-02-18 16:36:39 +0530

    init

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,24 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_user!
+	def index
+    @comment = Comment.all
+
+  end
+
+  def addcomment
+		@comment = current_user.comments.create(body: params[:body],time: params[:time], color: params[:color])#Comment.new
+    render :json => {status: "ok"}
+	end
+
+	# def create
+ #    	@user = User.find(params[:user_id])
+ #    	@comment = @user.comments.create(comment_params)
+ #  end
+
+   
+  	# private
+   #  def comment_params
+   #    params.require(:comment).permit(:commenter, :body)
+   #  end
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+App::Application.config.secret_key_base = '86f98ffb68951ee81831a0abec591cfab94028e46e277a75411af3ed0407f9a283a53ee282e827a391f3bfe024b2b91d307ba4fc66265dddff2349de3f1a0c51'
